e013732e050fa0a43ecc17ba8792c9d7f449cf0f
ZeynelErdiKarabulut <erdi_karabulut@yahoo.com.tr>
2020-12-04 17:46:37 +0300

    initial commit

---
+++ app/src/main/java/com/example/passwordvault/db/BankDao.kt
@@ -0,0 +1,25 @@
+package com.example.passwordvault.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+import com.example.passwordvault.model.BankDetailsItem
+
+/**
+ * Created by Zeynel Erdi on 07,July,2020
+ */
+
+@Dao
+interface BankDao {
+
+    @Insert
+    fun insertBankDetails(bankDetails : BankDetailsItem)
+
+    @Query("DELETE FROM BankDetailsTable WHERE bankAccNumber = :accNumber")
+    fun deleteBankDetails(accNumber : Long)
+
+    @Query("SELECT * FROM BankDetailsTable")
+    fun getAllBankDetails() : LiveData<List<BankDetailsItem>>
+
+}
---
+++ app/src/main/java/com/example/passwordvault/model/LoginDetailsItem.kt
@@ -0,0 +1,12 @@
+package com.example.passwordvault.model
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+/**
+ * Created by Zeynel Erdi on 01,July,2020
+ */
+@Entity(tableName = "LoginDetailsTable")
+data class LoginDetailsItem(@PrimaryKey var id : String, var loginName:String, var loginEmail : String, var loginPassword : String, var loginWebsite : String, var loginNotes : String, var loginCategory : String) {
+
+}
